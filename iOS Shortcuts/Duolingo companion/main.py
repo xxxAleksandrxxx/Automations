64b76aeb10797424d56e91c5b40e27d424717ee9
xxxAleksandrxxx <103126986+xxxAleksandrxxx@users.noreply.github.com>
2023-12-17 13:46:31 +0700

    Update main.py
    del prints form "•" in t:

--- iOS Shortcuts/Duolingo companion/main.py
+++ iOS Shortcuts/Duolingo companion/main.py
@@ -27,11 +27,11 @@ def process_text(input_text):
             pattern = f'(.+){v}'
             t = re.search(pattern, t).group(1)
 
-    # clean text from everything after •
-    if "•" in t:
-        print('"•" in t')
-    else:
-        print('"•" NOT in t:')
+    # # clean text from everything after •
+    # if "•" in t:
+    #     print('"•" in t')
+    # else:
+    #     print('"•" NOT in t:')
     while "•" in t:
         pattern = r'(.+[.?!]) {1,3}[•]'
         t = re.search(pattern, t).group(1)
