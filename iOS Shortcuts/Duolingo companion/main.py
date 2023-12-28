7e27a5679c22aceaaf161b864dff677b2b84a878
xxxAleksandrxxx <103126986+xxxAleksandrxxx@users.noreply.github.com>
2023-12-28 12:11:53 +0700

    Update main.py

--- iOS Shortcuts/Duolingo companion/main.py
+++ iOS Shortcuts/Duolingo companion/main.py
@@ -3,13 +3,15 @@ import requests
 import process_text
 import tts
 import asyncio
+from zipfile import ZipFile
 
 # create Flask app object
 app = Flask(__name__)
 
-# apply decorator 
+# apply decorators 
 @app.route('/clear', methods=['POST'])
-def api():
+def clear_text():
+    # expecting there was text key in json we got from request
     data = request.json
     if 'text' not in data:
         return jsonify({'error' : 'Missing "text" field'}), 400
@@ -18,21 +20,31 @@ def api():
     return jsonify({'result' : result})
 
 @app.route('/tts', methods=['POST'])
-def api_2():
-    # data = request.json
-    print('request.json:', request.json)
-    print('request.form:', request.form)
+def text_to_sound():
+    # expecting there were text,  key in json we got from request
+    data = request.json
+    print(data)
     # if 'text' not in data:
     #     return jsonify({'error' : 'Missing "text" field'}), 400
     # file_name = request.form['file_name']
-    # TEXT = "Hello World!"
-    TEXT = request.form['text']
-    VOICE = "en-GB-SoniaNeural"
-    # OUTPUT_FILE = "test.mp3"
-    OUTPUT_FILE = request.form['file_name']
-    # tts.run(TEXT, VOICE, OUTPUT_FILE)
-    # await tts.run(TEXT, VOICE, OUTPUT_FILE)  # 'await' outside async function
-    asyncio.run(tts.run(TEXT, VOICE, OUTPUT_FILE))
+    
+    # TEXT_1 = data['text'][0]
+    # VOICE_1 = data['voice'][0]
+    # OUTPUT_FILE_1 = data['file_name'][0]
+    # asyncio.run(tts.run(TEXT_1, VOICE_1, OUTPUT_FILE_1))
+
+    
+    FILE_0 = data['file_name'][0]
+    FILE_1 = data['file_name'][1]
+    asyncio.gather(
+        tts.run(data['text'][0], data['voice'][0], FILE_0),
+        tts.run(data['text'][1], data['voice'][1], FILE_1)
+    )
+    OUTPUT_FILE = 'audios.zip'
+    with ZipFile(OUTPUT_FILE, 'w') as z:
+        z.write(FILE_0)
+        z.write(FILE_1)
+    
     return send_file(OUTPUT_FILE, as_attachment=True)
 
 
