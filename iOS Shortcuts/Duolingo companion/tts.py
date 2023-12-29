d701d8437c244778acc462e3d713a33853f4156c
xxxAleksandrxxx <103126986+xxxAleksandrxxx@users.noreply.github.com>
2023-12-29 16:25:10 +0700

    Update tts.py
    add rate parameter

--- iOS Shortcuts/Duolingo companion/tts.py
+++ iOS Shortcuts/Duolingo companion/tts.py
@@ -1,6 +1,6 @@
 import asyncio
 import edge_tts
 
-async def run(text, voice, file_name) -> None:
-    communicate = edge_tts.Communicate(text, voice)
+async def run(text, voice, rate, file_name) -> None:
+    communicate = edge_tts.Communicate(text, voice, rate=rate)
     await communicate.save(file_name)
