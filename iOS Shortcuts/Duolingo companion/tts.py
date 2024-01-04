7675cd53fde2e31bfae6672fd7df280ed9e2455d
xxxAleksandrxxx <103126986+xxxAleksandrxxx@users.noreply.github.com>
2024-01-05 04:11:29 +0700

    Update tts.py
    returned back as trimming of communicate response didn't work inside memory.

--- iOS Shortcuts/Duolingo companion/tts.py
+++ iOS Shortcuts/Duolingo companion/tts.py
@@ -1,18 +1,6 @@
 import asyncio
 import edge_tts
-from pydub import AudioSegment
-from io import BytesIO
 
 async def run(text, voice, rate, file_name) -> None:
     communicate = edge_tts.Communicate(text, voice, rate=rate)
-    print(type(communicate))
-    # Trim received audio from start and from end
-    try:
-        audio_stream = BytesIO(communicate)
-        audio = AudioSegment.from_file(audio_stream)
-        audio_trimmed = audio[100:-960]
-        await audio_trimmed.export(file_name, format="mp3")
-    except:
-        print("Audio didn't trimmed")
-        await communicate.save(file_name)
-    # await communicate.save(file_name)
+    await communicate.save(file_name)
