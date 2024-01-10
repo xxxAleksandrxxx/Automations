0aa41132606ffce7523e0e9ae3b30a010886c040
xxxAleksandrxxx <103126986+xxxAleksandrxxx@users.noreply.github.com>
2024-01-10 11:21:49 +0700

    Update tts.py
    Add pitch argument

--- iOS Shortcuts/Duolingo companion/tts.py
+++ iOS Shortcuts/Duolingo companion/tts.py
@@ -1,6 +1,6 @@
 import asyncio
 import edge_tts
 
-async def run(text, voice, rate, file_name) -> None:
+async def run(text, voice, rate, file_name, pitch="+0Hz") -> None:
     communicate = edge_tts.Communicate(text, voice, rate=rate)
     await communicate.save(file_name)
